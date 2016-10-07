d020a0c4ac60c823f36d5678051edd2cfaf0fba1
lawrencekhlim <lawrencekhlim@gmail.com>
2016-10-06 21:13:01 -0700

    New Update on Vim

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+function openFirebase() {
+    var config = {
+        apiKey: "AIzaSyCt21vzwMQozqSirpMkWewElYNy0XMPi-A",
+        authDomain: "bspoker-a0a5c.firebaseapp.com",
+        databaseURL: "https://bspoker-a0a5c.firebaseio.com",
+        storageBucket: "",
+        messagingSenderId: "173303633528"
+    };
+    firebase.initializeApp(config);
+}
